ad25b99dd6fbb7d086274affd29d67abedd18abe
Diego Calciolari Gordiano <81247672+ViniPierre@users.noreply.github.com>
2022-05-06 12:42:37 -0300

    Aprendendo arvore

---
+++ ArvoreBinaria/ArvoreBinaria.java
@@ -0,0 +1,14 @@
+public class ArvoreBinaria {
+    
+    private No raiz;
+
+    public void inserir(int valor) {
+        if  (raiz == null) {
+        raiz = new No(valor);
+        } else {
+            raiz.inserirNo(valor);
+        }
+    }
+
+
+}
---
+++ ArvoreBinaria/No.java
@@ -0,0 +1,14 @@
+public class No {
+    int dado;
+    No esq;
+    No dir;
+
+    public No (int dado) {
+        super();
+        this.dado = dado;
+    }
+
+    public void inserirNo(int valor) {
+        
+    }
+}
